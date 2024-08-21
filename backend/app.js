6104e3042f6f49f2976736d419b68fbc4318b03d
Shaaf Salman <iamshaafsalman@gmail.com>
2024-08-21 21:44:15 +0500

    login cors -> domain

--- backend/app.js
+++ backend/app.js
@@ -5,16 +5,30 @@ const PORT = process.env.PORT || 1122;
 
 const db = require('./db'); 
 
-app.use(cors()); 
-app.use(express.json());
+const corsOptions = {
+  origin: 'https://lumuslights.com', 
+  methods: 'GET, POST, PUT, DELETE, OPTIONS',
+  allowedHeaders: 'Content-Type, Authorization',
+};
+
+app.use(cors(corsOptions));
+app.use(express.json()); 
 
 const authRoutes = require('./Route/authRoute');
 app.use('/api', authRoutes);
 
+// Basic route for testing
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+// Global error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
